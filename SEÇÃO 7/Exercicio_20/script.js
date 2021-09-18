b18e85e259ca9bec020d66d46693deef0518a261
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-18 20:07:47 -0300

    Update script.js

--- SEÇÃO 7/Exercicio_20/script.js
+++ SEÇÃO 7/Exercicio_20/script.js
@@ -3,12 +3,3 @@
 // >18 não pode entrar na auto escola. Imprimir mensagem informando
 // executar função nos dois casos
 
-function idade(num) {
-  let perguntaIdade = prompt("Qual sua idade?");
-  if (num < 18) {
-    console.log("Não autorizado a entrar na auto escola!");
-  } else num >= 18;
-  {
-    console.log("Autorizado a entrar na auto escola!");
-  }
-}
