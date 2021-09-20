c3ea1fee969da3891ceac2a40e5def59099c8db4
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-20 15:49:23 -0300

    Update script.js

--- SEÇÃO 7/Exercicio_22/script.js
+++ SEÇÃO 7/Exercicio_22/script.js
@@ -1,8 +1,8 @@
 // Escrever uma função que recebe um nº negativo e retorne um nº positivo
 // Dica: utilize a função Math.abs
 
-function numero(x) {
+function numeroPisitivo(x) {
   return Math.abs(x);
   console.log(x);
 }
-console.log(numero(-15));
+console.log(numeroPositivo(-15));
