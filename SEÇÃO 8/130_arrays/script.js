b8156c269a03c457ae4555b57b69ab8e93a90754
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-23 16:56:02 -0300

    Add files via upload

---
+++ SEÇÃO 8/130_arrays/script.js
@@ -0,0 +1,9 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ["Eduarda", "Lohan", "Vitor"];
+let boll = [true, false, true];
+let misturado = [1, "Cris", true];
+
+console.log(arr);
+console.log(nomes);
+console.log(boll);
+console.log(misturado);
