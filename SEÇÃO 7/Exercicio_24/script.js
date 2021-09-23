183adf63215dfc80192e291368ad655d41be2b1d
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-23 15:15:28 -0300

    Add files via upload

---
+++ SEÇÃO 7/Exercicio_24/script.js
@@ -0,0 +1,9 @@
+// função que receba dois números: o primeiro é a base e o segundo a potência;
+// Depois fazer essa operação e retorne o resultado;
+
+function potencia(base, exp) {
+  return Math.pow(base, exp); //OU return base ** exp;
+}
+console.log(potencia(2, 5));
+console.log(potencia(2, 3));
+console.log(potencia(3, 3));
