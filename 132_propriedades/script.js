feec8582e5f9edc694b65e45047ff390258457ba
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-24 15:53:03 -0300

    Add files via upload

---
+++ 132_propriedades/script.js
@@ -0,0 +1,9 @@
+let nome = "Lohan";
+
+console.log(nome.length); // Conta o nº de caracteres ('lohan' = 5)
+
+let numeros = [1, 23, 34, 5, 6, 7, 8];
+
+console.log(numeros.length); // conta quantidades de caracteres em let numeros (= 7)
+console.log(numeros["length"]); // conta quantidades de caracteres em let numeros (= 7)
+console.log(numeros[1]); // apresenta caracter na posição [1] = 23
