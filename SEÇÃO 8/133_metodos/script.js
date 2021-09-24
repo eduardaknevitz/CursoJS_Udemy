347cec6338e13600594e4334eaeddc4496f0c142
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-24 16:15:05 -0300

    Add files via upload

---
+++ SEÇÃO 8/133_metodos/script.js
@@ -0,0 +1,6 @@
+let marca = "nike";
+console.log(marca.toUpperCase()); // console: NIKE
+
+let marca2 = marca.toUpperCase();
+console.log(marca2.toLowerCase()); // console: nike
+console.log(typeof marca.toLowerCase); // retorna como function
