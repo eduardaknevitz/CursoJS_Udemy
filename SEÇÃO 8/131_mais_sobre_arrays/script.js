1e4bcf990e9b747312ac7decfc027c9824d2a54b
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-24 10:00:02 -0300

    Add files via upload

---
+++ SEÇÃO 8/131_mais_sobre_arrays/script.js
@@ -0,0 +1,12 @@
+// Acessar array pelo seu índice, por exemplo: arr[1];
+// Lembrete: o primeiro índice sempre é o 0, ou seja, para acessar o primeiro elemento: arr[0];
+
+let arr = [1, 4, 5, 6, 7];
+let nomes = ["Eduarda", "Lohan", "Vitor"];
+let boll = [true, false, true];
+let misturado = [1, "Cris", true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(boll[2]);
+console.log(arr[arr.length - 1]);
