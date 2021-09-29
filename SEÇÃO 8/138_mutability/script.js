460d9d0266ae9efd931b6b8b238547d91e084ea8
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-29 15:27:00 -0300

    Add files via upload

---
+++ SEÇÃO 8/138_mutability/script.js
@@ -0,0 +1,21 @@
+let pessoa = {
+  nome: "Matheus",
+};
+
+let pessoa2 = pessoa;
+
+let pessoa3 = {
+  nome: "Matheus",
+};
+
+console.log(pessoa == pessoa2); // true
+console.log(pessoa3 == pessoa); // false
+console.log(pessoa3 == pessoa2); // false
+
+pessoa2.nome = "Pedro";
+
+console.log(pessoa.nome);
+
+pessoa.nome = "Maria";
+
+console.log(pessoa2.nome);
