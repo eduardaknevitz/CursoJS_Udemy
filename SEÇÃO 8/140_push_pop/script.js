c7637b49c2c4b2701bc24567c0083e4d276dd228
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-29 16:59:08 -0300

    Add files via upload

---
+++ SEÇÃO 8/140_push_pop/script.js
@@ -0,0 +1,15 @@
+let nomes = ["Eduarda", "Lohan", "Pedro"];
+
+console.log(nomes); //['Eduarda', 'Lohan', 'Pedro']
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido); // Pedro
+console.log(nomes); //['Eduarda', 'Lohan']
+
+nomes.push("Vitor");
+
+console.log(nomes); //['Eduarda', 'Lohan', 'Vitor']
+
+// pop = remove o elemento do fim do array
+// push = adiciona um elemento no fim do array
