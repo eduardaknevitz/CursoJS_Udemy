fa725383f6d70f438dd6b796bf5d21b030f04df9
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-30 14:48:08 -0300

    Add files via upload

---
+++ SEÇÃO 8/149_split/script.js
@@ -0,0 +1,8 @@
+let frase = "Testando o método split";
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+console.log(palavras);
+
+let produtos = "Banana; Maçã; Jaca; Pera";
+console.log(produtos.split(";"));
