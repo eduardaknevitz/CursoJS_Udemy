30ae86fd463ba39dee8e945e1eb8445b1fdd2d2a
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-29 23:50:03 -0300

    Add files via upload

---
+++ SEÇÃO 8/142_indexOf_lastIndexOf/script.js
@@ -0,0 +1,7 @@
+let nums = [5, 6, 2, 4, 9, 6, 2];
+
+console.log(nums.indexOf(2));
+console.log(nums.lastIndexOf(2));
+
+//indexOf: encontra o índice de um determinado elemento;
+// lastIndexOf: encontrar o último índice de um elemento;
