578e183a1719c42e1b2fe5c729c28286cf8f5288
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-09-30 10:25:44 -0300

    Add files via upload

---
+++ SEÇÃO 8/144_forEach/script.js
@@ -0,0 +1,7 @@
+let nomes = ["Eduarda", "Lohan", "Vitor", "Ethel", "Everton"];
+
+nomes.forEach((nome) => {
+  console.log("O nome é " + nome);
+});
+
+//trabalha com a palavra no singular - mais declarativo - JS interpreta como paramentro
