e136687b1b9421c5e8dfff7e9856e112bcd871b9
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-10-02 22:51:27 -0300

    Add files via upload

---
+++ SEÇÃO 9/Exercicio_32/script.js
@@ -0,0 +1,9 @@
+// Criar um array com 5 elemento;
+// Fazer uma interacao entre eles e imprimir no console o valor;
+// loop em arrays
+
+let nomes = ["Eduarda", "Lohan", "Vitor", "Ethel", "Everton"];
+
+for (let i = 0; i < nomes.length; i++) {
+  console.log(nomes[i]);
+}
---
+++ SEÇÃO 9/Exercicio_33/script.js
@@ -0,0 +1,11 @@
+// Criar um JSON com 3 propriedades;
+// Atribuir ele a uma varivel;
+// Acessar as propiedades imprimindo no console;
+
+let pessoa = {
+  nome: "Eduarda",
+  idade: 23,
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
