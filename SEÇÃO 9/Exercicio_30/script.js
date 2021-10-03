c65d917245e4ca840188f1013d3ac41faacf34c4
Eduarda Knevitz <90861901+eduardaknevitz@users.noreply.github.com>
2021-10-02 22:09:00 -0300

    Add files via upload

---
+++ SEÇÃO 9/Exercicio_30/script.js
@@ -0,0 +1,8 @@
+// Criar array com 5 nomes e verificar se existe no array;
+// Se existir, imprimir mensagem no console;
+
+let nomes = ["Eduarda", "Lohan", "Vitor", "Ethel", "Everton"];
+
+if (nomes.includes("Eduarda")) {
+  console.log("O nome foi encontrado!");
+}
